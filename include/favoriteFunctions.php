0c728990f93de0c6afc76520aa4faafab8c6d3da
Cory Weinstein <cweinstein@stanford.edu>
2012-11-08 20:36:06 -0800

    Favorites displaying favorite doctors

--- include/favoriteFunctions.php
+++ include/favoriteFunctions.php
@@ -1,30 +1,27 @@
 <?php
 
-
+include("include/config.php");
 
 
 function addDoctors(){
 	
-
-						
-						include("include/config.php");
 						$name = $_COOKIE["name"];
 						$query = "SELECT * FROM favorites WHERE cookieName = '$name'";
 						$result = mysql_query($query);
 						if ($result && mysql_num_rows($result) != 0) {
 							
 							while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    							$curDocId = $row["DoctorID"];
+    							$id = $row["DoctorID"];
     							
-    							$queryTwo = "SELECT * FROM doctors WHERE id = $curDocID";
+    							$queryTwo = "SELECT * FROM doctors WHERE id = '".$id."'";
+    							
     							$resultTwo = mysql_query($queryTwo);
+    							$rowTwo = mysql_fetch_array($resultTwo, MYSQL_ASSOC);
     							
-    							$rowTwo = mysql_fetch_assoc($resultTwo);
-    							echo '$rowTwo["name"]';
     							echo '<li>';
-    							
-    							echo '<a href = "profile.php?id='.$curDocId.'" data-transition = "slide"';
-    							 echo ' <img src = '.$rowTwo["image"].' class = "profilePic">';
+    							 echo "<img src = '".$rowTwo["image"]."'class = 'profilePic'>";
+    							echo '<a href = "profile.php?id='.$id.'" data-transition = "slide"';
+    								
     							  echo '<h3> '.$rowTwo["name"].' </h3>';
     							  echo '<p> Phone: '.$rowTwo["phone"].' <br> Hours: '.$rowTwo["hours"].' </br> </p>';
     							  echo '</a>';
